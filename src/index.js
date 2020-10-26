840d0175dfef9b911799d05742ed434886220020
Henadzi Hirylovich <Gir.gen2019@gmail.com>
2020-10-26 19:45:57 +0300

    done and chenge task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+  const arr = [];
+
+  if (matrix === undefined || matrix.length === 0){
+    return [];
+  } 
+
+  for (let i = 0 ; i < matrix.length ; i += 1) {
+    if (i % 2 == 0) {
+      for (let j = 0; j < matrix[i].length; j += 1) {
+        arr.push(matrix[i][j]);
+      }
+    } else {
+      const reversedMatrix = matrix[i].reverse();
+      for (let j = 0; j < matrix[i].length; j += 1) {
+        arr.push(reversedMatrix[j]);
+      }
+    }
+  }
+  return arr;
+};
